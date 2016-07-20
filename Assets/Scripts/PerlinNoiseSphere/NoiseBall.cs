88234469094a76299e785d1aa1726e6ad9f361f1
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2016-07-20 19:19:43 +0900

    Added perlin noise demo.

---
+++ Assets/Scripts/PerlinNoiseSphere/NoiseBall.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NoiseBall : MonoBehaviour
+{
+
+	private Vector3 initialPosition;
+	private List<LineRenderer> lineStarting = new List<LineRenderer>();
+	private List<LineRenderer> lineEnding = new List<LineRenderer>();
+	private Renderer rend;
+	public Color Color { get; set; }
+
+	// Use this for initialization
+	void Start ()
+	{
+		initialPosition = transform.position;
+		this.rend = GetComponent<Renderer> ();
+		this.rend.material.SetColor("_EmissionColor", this.Color);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float t = Time.time;
+		float magnitude = t / 60f;
+//		float magnitude = 0.2f;
+		var s = initialPosition;
+		var noise = (Mathf.PerlinNoise (s.x + t, s.z + t) - 0.5f) * magnitude + 1f;
+		var newPos = s * noise;
+		transform.position = newPos;
+
+		foreach (var lr in lineStarting) {
+			lr.SetPosition (0, newPos);
+		}
+		foreach (var lr in lineEnding) {
+			lr.SetPosition (1, newPos);
+		}
+	}
+
+	public void AddLineStarting(LineRenderer lr) {
+		lineStarting.Add (lr);
+	}
+
+	public void AddLineEnding(LineRenderer lr) {
+		lineEnding.Add (lr);
+	}
+}
---
+++ Assets/Scripts/PerlinNoiseSphere/PerlinNoiseSphere.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PerlinNoiseSphere : MonoBehaviour
+{
+
+	public GameObject ballPrefab;
+	public GameObject linePrefab;
+	private const float R = 5f;
+	private const int NUM = 10000;
+	private const float THRESHOLD = 0.0f;
+	private const float FREQUENCY = 3f;
+	private readonly int[] startIndexes = { 0, 1, 2 };
+	private readonly int[] endIndexes = { 1, 2, 0 };
+
+	// Use this for initialization
+	void Start ()
+	{
+		Init (true, NUM / 2);
+//		Init (false, NUM / 2);
+	}
+
+	void Init (bool upside, int num)
+	{
+		List<Vector2> points2d = new List<Vector2> ();
+		List<GameObject> balls = new List<GameObject> ();
+		int count = 0;
+		while (true) {
+			var rand = Random.onUnitSphere;
+			rand.y = Mathf.Abs (rand.y);
+			if (!upside) {
+				rand.y = -rand.y;
+			}
+			var noise = Mathf.PerlinNoise (FREQUENCY * rand.x, FREQUENCY * rand.z);
+			if (noise > THRESHOLD) {
+				var pos = R * rand;
+				var obj = Instantiate (ballPrefab, pos, Quaternion.identity) as GameObject;
+				var cv = (noise - THRESHOLD) / (1f - THRESHOLD);
+				obj.GetComponent<NoiseBall> ().Color = Color.HSVToRGB (cv, 1f, cv);
+				balls.Add (obj);
+				points2d.Add (new Vector2 (rand.x, rand.z));
+				if (++count >= num) {
+					break;
+				}
+			}
+		}
+
+		Debug.Log (string.Format ("{0} objectes instantiated.", points2d.Count));
+
+		var triangulator = new Triangulator ();
+		var triangles = triangulator.TriangulatePolygon (points2d.ToArray ());
+
+		for (var i = 0; i < triangles.Length; i += 3) {
+			for (int j = 0; j < 3; j++) {
+				var l = triangles [i + startIndexes [j]];
+				var r = triangles [i + endIndexes [j]];
+
+				var line = Instantiate (linePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+				var lineRenderer = line.GetComponent<LineRenderer> ();
+
+				lineRenderer.SetPosition (0, balls [l].transform.position);
+				lineRenderer.SetPosition (1, balls [r].transform.position);
+				balls [l].GetComponent<NoiseBall> ().AddLineStarting (lineRenderer);
+				balls [r].GetComponent<NoiseBall> ().AddLineEnding (lineRenderer);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		var t = Time.time;
+		var x = 10f * Mathf.Cos (t / 10f);
+		var z = 10f * Mathf.Sin (t / 10f);
+		var campos = Camera.main.transform.position;
+		campos.x = x;
+		campos.z = z;
+		campos.y = 8f;
+
+		Camera.main.transform.position = campos;
+		Camera.main.transform.LookAt (Vector3.zero);
+	}
+}
