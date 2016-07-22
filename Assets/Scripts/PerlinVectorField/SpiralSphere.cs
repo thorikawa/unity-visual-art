b2cb9861135084ff458cf390d5af9dbc2234c7c8
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2016-07-23 03:12:17 +0900

    Add perlin vector field

---
+++ Assets/Scripts/PerlinVectorField/SpiralSphere.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SpiralSphere : MonoBehaviour {
+
+	Rigidbody rb;
+	private const float R = 10f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var t = Time.time;
+		var theta = t / 1.619f;
+		var phi = t / 0.438f;
+		float x1 = R * Mathf.Sin (theta) * Mathf.Cos (phi);
+		float y1 = R * Mathf.Sin (theta) * Mathf.Sin (phi);
+		float z1 = R * Mathf.Cos (theta);
+		var pos = transform.position;
+		pos.Set (x1, y1, z1);
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/PerlinVectorField/TronTrail.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Rigidbody))]
+public class TronTrail : MonoBehaviour
+{
+
+	public Vector3 InitialDirection = new Vector3 (1f, 0f, 0f);
+	public float INITIAL_SPEED = 3.0f;
+	private Rigidbody rb;
+	public float NOISE_SCALE = 0.1f;
+	public float EFFECT_SCALE = 20f;
+	public float GRAVITY_FACTOR = 0f;
+	public float GRAVITY_DISTANCE_SCALE = 0.2f;
+	public GameObject engineObject;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = InitialDirection * INITIAL_SPEED;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var t = Time.time;
+		float dist = transform.position.magnitude;
+		var gravity = GRAVITY_FACTOR *  Mathf.Pow(dist * GRAVITY_DISTANCE_SCALE, 2f) * (-rb.position.normalized);
+
+		Vector3 noise = Vector3.zero;
+		if (engineObject != null) {
+			noise = Perlin.NoiseVec (engineObject.transform.position * NOISE_SCALE);
+			transform.position = noise * 10f;
+		} else {
+//			noise = Perlin.NoiseVec (transform.position * NOISE_SCALE);
+//			rb.AddForce (noise * EFFECT_SCALE + gravity);
+//			rb.velocity = rb.velocity.normalized * 10f;
+//			rb.velocity = Random.onUnitSphere;
+		}
+	}
+}
